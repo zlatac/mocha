19ad412fc442903fe75522cebd65370c188e2699
zlatac <[idahosa1adaghe@hotmail.ca]>
2017-08-21 19:39:37 -0400

    ionic service trial

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var myapp = angular.module('starter', ['ionic'])
+var myapp = angular.module('starter', ['ionic','ionic.cloud'])
+
+.config(function($ionicCloudProvider) {
+  $ionicCloudProvider.init({
+    "core": {
+      "app_id": "c2f7c8b5"
+    }
+  });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
