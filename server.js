2542d51bf23271821c8c4023f47168088951d199
zlatac <[idahosa1adaghe@hotmail.ca]>
2018-01-19 15:05:18 -0500

    server

--- server.js
+++ server.js
@@ -15,11 +15,12 @@
 //  console.log('listening to Port', app.get('port'));
 // });
 
+//stock server
 var express = require('express');
 var path = require('path')
 var app = express();
 app.use(express.static(path.resolve(__dirname, 'www')));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.listen(app.get('port'), function() {
  console.log('listening to Port', app.get('port'));
 });
