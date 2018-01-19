12a847420a27d10d938b0d277ff18ec650b1b752
zlatac <[idahosa1adaghe@hotmail.ca]>
2018-01-19 14:51:17 -0500

    updayte

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-var express = require('express');
-var path = require('path')
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-server.listen(8000);
+// var express = require('express');
+// var path = require('path')
+// var app = express();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+// app.set('port', process.env.PORT || 8000);
+// server.listen(app.get('port'));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/www/index.html');
-   });
-app.use(express.static(path.resolve(__dirname, 'www')));
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/www/index.html');
+//    });
+// app.use(express.static(path.resolve(__dirname, 'www')));
 //app.set('port', process.env.PORT || 8000);
 // app.listen(app.get('port'), function() {
 //  console.log('listening to Port', app.get('port'));
 // });
+
+var express = require('express');
+var path = require('path')
+var app = express();
+app.use(express.static(path.resolve(__dirname, 'www')));
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+ console.log('listening to Port', app.get('port'));
+});
 
 //websocket testing code
 io.on('connection', function(client) {  
