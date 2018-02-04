86ff15b09751d0bae02bb1f157cf252c39ac8fff
zlatac <[idahosa1adaghe@hotmail.ca]>
2018-02-04 04:03:42 -0500

    picpuzzle update

--- www/js/main.js
+++ www/js/main.js
@@ -344,7 +344,7 @@ var myapp = angular.module('starter', ['ionic'])
         params: {mode: 'ryerson'},
         cache: false
       });
-    $urlRouterProvider.otherwise('/boropuzzle');
+    $urlRouterProvider.otherwise('/');
 })
 
 .run(function($ionicPlatform) {
